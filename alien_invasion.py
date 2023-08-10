a6456e0c4c0528c567f63a80aab63c03a3f78c0d
Reece <rdavis001@hotmail.com>
2023-08-10 14:27:33 +0100

    bullet complete

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,11 +1,14 @@
 import sys
 
+import random
+
 import pygame
 
 from settings import Settings
 
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -23,6 +26,9 @@ class AlienInvasion:
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        
+        self._create_fleet()
         
         #set the background colour.
     def run_game(self):
@@ -31,7 +37,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets() 
-            
+            self._update_aliens()
             self._update_screen()
             
     def _check_events(self):
@@ -69,7 +75,7 @@ class AlienInvasion:
             self.bullets.add(new_bullet)
     
     def _update_bullets(self):
-        """Update position of bullets and get rid of old bbullets."""
+        """Update position of bullets and get rid of old bullets."""
         #update bullet positions
         self.bullets.update()
         
@@ -78,6 +84,58 @@ class AlienInvasion:
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
                 print(len(self.bullets))
+    
+    def _create_fleet(self):
+        """Create the fleet of aliens"""
+        #create a fleet and find the number of aliens in a row.
+        #spacing between each alien is equal to one alien width.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+        
+        #determine the number of rows of aliens that fit on the screen
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+        
+        #create full fleet of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+    
+    def _create_alien(self, alien_number, row_number):
+        """Create an alien and place it in the row"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+    
+    def _check_fleet_edges(self):
+        """Respond appropriatley if aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleets direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+        
+            
+        
+    def _update_aliens(self):
+        """
+        Check if the fleet is at an edge,
+        then update the position of all the aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
                 
                     
     def _update_screen(self):
@@ -86,6 +144,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
             
             # Make the most recently drawn screen visible.
         pygame.display.flip()
--- settings.py
+++ settings.py
@@ -18,3 +18,11 @@ class Settings:
         self.bullet_colour = (255, 165, 0)
         self.bullets_allowed = 3
         
+        #alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        #fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+        
+        
+        
